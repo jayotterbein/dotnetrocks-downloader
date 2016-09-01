17f670ef938f7344f7f37a619d0963c2e176eb63
Jay Otterbein <jayotterbein@users.noreply.github.com>
2016-09-01 15:03:23 -0500

    modified to be python3 and added setting of modified time of the mp3 to the date in the rss feed

--- rssDownloader.py
+++ rssDownloader.py
@@ -1,13 +1,13 @@
 import feedparser;
-from datetime import datetime, timedelta;
+from datetime import datetime, timedelta, timezone;
 from time import mktime;
-from os import path, makedirs, listdir;
-from urlparse import urlparse;
-from urllib import urlretrieve;
+from os import path, makedirs, listdir, utime;
+from urllib.request import urlretrieve;
+from urllib.parse import urlparse;
 
 # .NET Rocks publishes at midnight eastern time
 
-url ='http://www.pwop.com/feed.aspx?show=dotnetrocks&filetype=master'
+url = 'http://www.pwop.com/feed.aspx?show=dotnetrocks&filetype=master'
 outputDir = '/Media/DotNetRocks'
 
 if not path.exists(outputDir):
@@ -22,6 +22,9 @@ for item in parsedFeed.entries:
     if published >= oldestDesired:
         for e in item.enclosures:
             filename = path.basename(urlparse(e.url).path)
+            fullpath = path.join(outputDir, filename)
             if filename not in files:
-                print 'Downloading ' + filename
-                urlretrieve(e.url, path.join(outputDir, filename))
+                print('Downloading ' + filename)
+                urlretrieve(e.url, fullpath)
+            publishedTs = published.replace(tzinfo=timezone.utc).timestamp()
+            utime(fullpath, (publishedTs, publishedTs))
